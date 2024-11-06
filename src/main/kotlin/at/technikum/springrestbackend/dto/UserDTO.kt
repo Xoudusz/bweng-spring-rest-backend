1a4ac8bcaaa65c8827d8b6e1663b9910522ce988
David Tinhofer <da@w23.at>
2024-11-06 20:49:45 +0100

    changed password validation added WebConfig so the frontend can access the api

--- src/main/kotlin/at/technikum/springrestbackend/dto/UserDTO.kt
+++ src/main/kotlin/at/technikum/springrestbackend/dto/UserDTO.kt
@@ -17,10 +17,10 @@ data class UserDTO(
     val email: String,
 
     @field:NotBlank(message = "Password is required")
-    @field:Size(min = 8, message = "Password must be at least 8 characters")
-    @field:Pattern(
-        regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}",
-        message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
-    )
+//    @field:Size(min = 8, message = "Password must be at least 8 characters")
+//    @field:Pattern(
+//        regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}",
+//        message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
+//    )
     val passwordHash: String
 )
