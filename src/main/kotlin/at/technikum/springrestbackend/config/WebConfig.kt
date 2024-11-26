f414fc7109382a9f1d24890768a80c04b5c8759e
David Tinhofer <da@w23.at>
2024-11-26 20:15:04 +0100

    you can now also use the email in the /api/auth endpoint instead of only username

--- src/main/kotlin/at/technikum/springrestbackend/config/WebConfig.kt
+++ src/main/kotlin/at/technikum/springrestbackend/config/WebConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:8082")
+            .allowedOrigins("http://localhost:8081")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
