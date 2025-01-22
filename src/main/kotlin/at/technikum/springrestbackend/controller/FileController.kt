9a8d2a606214250c933b5d612dba8d0c7eef6398
xoudusz <da@w23.at>
2025-01-22 15:43:23 +0100

    added more permission checks

--- src/main/kotlin/at/technikum/springrestbackend/controller/FileController.kt
+++ src/main/kotlin/at/technikum/springrestbackend/controller/FileController.kt
@@ -5,6 +5,7 @@ import org.springframework.core.io.InputStreamResource
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
@@ -16,6 +17,7 @@ data class FileUploadResponse(val uuid: String, val message: String)
 class FileController(
     private val fileService: FileService
 ) {
+    @PreAuthorize("isAuthenticated()")
     @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     @ResponseStatus(HttpStatus.CREATED)
     fun uploadFile(@RequestPart("file") file: MultipartFile): FileUploadResponse {
@@ -28,7 +30,7 @@ class FileController(
         return FileUploadResponse(uuid = uuid, message = "File uploaded successfully.")
     }
 
-
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{uuid}")
     fun downloadFile(@PathVariable uuid: String): ResponseEntity<InputStreamResource> {
         val fileDownloadResponse = fileService.downloadFile(uuid)
@@ -43,6 +45,7 @@ class FileController(
             .body(fileDownloadResponse.resource)
     }
 
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{uuid}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun deleteFile(@PathVariable uuid: String) {
--- src/main/kotlin/at/technikum/springrestbackend/controller/FollowController.kt
+++ src/main/kotlin/at/technikum/springrestbackend/controller/FollowController.kt
@@ -2,6 +2,7 @@ package at.technikum.springrestbackend.controller
 
 import at.technikum.springrestbackend.dto.FollowDTO
 import at.technikum.springrestbackend.service.FollowService
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import java.util.UUID
 
@@ -11,18 +12,22 @@ class FollowController(
     private val followService: FollowService
 ) {
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping
     fun followUser(@RequestBody followDTO: FollowDTO) =
         followService.followUser(followDTO.followerId, followDTO.followingId)
 
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping
     fun unfollowUser(@RequestBody followDTO: FollowDTO) =
         followService.unfollowUser(followDTO.followerId, followDTO.followingId)
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/followers/{userId}")
     fun getFollowers(@PathVariable userId: UUID) =
         followService.getFollowers(userId)
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/following/{userId}")
     fun getFollowing(@PathVariable userId: UUID) =
         followService.getFollowing(userId)
